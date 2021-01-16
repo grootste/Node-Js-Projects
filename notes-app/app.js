1c4cdbc32b41f93da1cc39385015531d3b7b6c2b
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-01-16 22:41:04 +0530

    Update app.js

--- notes-app/app.js
+++ notes-app/app.js
@@ -6,3 +6,11 @@ console.log(msg)
 
 const greenmsg = chalk.blue.inverse.bold('Sucesssss!!!!!!!!!')
 console.log(greenmsg)
+
+const command = process.argv[2]
+
+if(command == 'add'){
+    console.log('Adding note')
+} else if( command == 'remove'){
+    console.log('Removing note!!')
+}
