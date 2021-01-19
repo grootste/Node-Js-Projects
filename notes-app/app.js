17fcecdded9e3d0a0214902d513d18ba0a80259b
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-01-19 14:05:41 +0530

    minor

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,58 @@
 const chalk = require('chalk')
+const { demandOption, argv } = require('yargs')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const msg = getNotes('My name is Baibhav !!!!!!')
-console.log(msg)
 
-const greenmsg = chalk.blue.inverse.bold('Sucesssss!!!!!!!!!')
-console.log(greenmsg)
+// custom version
 
-const command = process.argv[2]
+yargs.version('1.1.0')
 
-if(command == 'add'){
-    console.log('Adding note')
-} else if( command == 'remove'){
-    console.log('Removing note!!')
-}
+
+//create add comand
+yargs.command({
+    command: 'add',
+    describe : 'Add a new note',
+    builder:{
+        title : {
+            describe : 'Note title',
+            demandOption : true,
+            type: 'string'
+        }
+    },
+    handler : function () {
+        console.log('Title:' + argv.title)
+    }
+})
+
+// create remove command
+yargs.command({
+    command : 'remove',
+    describe : 'Removing the note ',
+    handler :  function() {
+        console.log('Removing a note')
+    }
+    
+})
+
+//create read
+yargs.command({
+    command: 'read',
+    describe: 'Read the note',
+    handler: function(){
+        console.log('Reading the note')
+    }
+})
+
+//create list
+yargs.command({
+    command:'list',
+    describe:'Listing the note',
+    handler : function (){
+        console.log('Listing the note')
+    }
+})
+
+//add, remove, read, list
+yargs.parse()
+
