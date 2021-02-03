10bf9548148920bc8546cc6cd559c28cf1fbc893
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-02-04 00:56:48 +0530

    minor

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,7 +1,30 @@
-const book = {
-    title: 'Ego is the enemey',
-    author: 'Rayan Holiday'
-}
+const fs =  require('fs')
 
-const bookJSON = JSON.stringify(book)
+const book = {
+    "name": "Andrew",
+    "planet": "Earth",
+    "age" : 27
+}
+  
+//const bookJSON = JSON.stringify(book)
+
+
+
+//const dataBuffer = fs.readFileSync('1-json.json')
+
+const dataBuffer = fs.readFileSync('data.json')
+
+
+
+const dataJSON =  dataBuffer.toString()
+
+const data = JSON.parse(dataJSON)
+
+data.name="Baibhav"
+data.planet = "Earth"
+data.age ="23"
+
+
+const bookJSON = JSON.stringify(data)
+fs.writeFileSync('data.json', bookJSON)
 console.log(bookJSON)
