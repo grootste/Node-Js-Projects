4167db605a7b8729fdb01f73fb1fc14c4ee8a138
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-03-02 15:50:00 +0530

    setup

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,6 +2,15 @@ console.log('starting')
 
 
 
+setTimeout(() =>{
+
+    console.log('2 secound Timer')
+}, 2000)
+
+setTimeout(() =>{
+
+    console.log('0 secound Timer')
+}, 0)
 
 
 console.log('Stopping')
