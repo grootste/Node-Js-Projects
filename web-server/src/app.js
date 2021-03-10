97835831d0129b7afc22962dbb5e300dbaef7f2b
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-03-10 21:45:45 +0530

    init

---
+++ web-server/src/app.js
@@ -0,0 +1,29 @@
+const path = require ('path')
+const  express = require('express')
+
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../src')
+
+app.set ('view engine', 'hbs')
+app.use(express.static(publicDirectoryPath))
+
+
+
+app.get('', (req, res) =>{
+    res.render('index')
+})
+
+
+
+app.get('/weather', (req, res) => {
+    res.send({
+        forecast: 'It is snowing',
+        location: 'philadelphia'
+    })
+})
+
+app.listen(3000, () =>{
+    console.log('Server is up on port 3000.')
+
+} )
