5f2effd82d2139fe2ebb6e61d39c1615e474bcd8
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-03-14 23:18:16 +0530

    frontend of weather app

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,49 @@
 const path = require ('path')
 const  express = require('express')
+// const { allowedNodeEnvironmentFlags } = require('process')
+
+const  hbs = require('hbs')
+const { title } = require('process')
 
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+//Define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+//Setup handlebars engine and views location
 app.set ('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 
 
 app.get('', (req, res) =>{
     res.render('index', {
-        
+        title: 'Weather App',
+        name: 'Baibhav Singh'
     })
 })
 
+app.get('/about', (req, res) =>{
+    res.render('about', {
+        title: 'About Me',
+        name: 'Baibhav Singh'
+    })
+})
 
+app.get('/help', (req, res) =>{
+    res.render('help', {
+        helpText: ' This is the  help message to the User',
+        title:'Help',
+        name: 'Baibhav Singh'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
@@ -24,6 +51,23 @@ app.get('/weather', (req, res) => {
         location: 'philadelphia'
     })
 })
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        name: 'Baibhav Singh',
+        errorMessage: 'Help article not found.'
+    })
+})
+
+app.get('*',(req, res) =>{
+    res.render('404', {
+        title: '404',
+        name: 'Baibhav Singh',
+        errorMessage: 'Page not found'
+    })
+})
+
 
 app.listen(3000, () =>{
     console.log('Server is up on port 3000.')
