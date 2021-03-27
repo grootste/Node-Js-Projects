96fd714f24d5118823e24a701ef1fb5eda601eaa
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-03-27 12:53:01 +0530

    update

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require ('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser : true,
@@ -7,31 +8,46 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 
 const user =mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true
     },
     age :{
-        type: Number
+        type: Number,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be a postive number')
+            }
+        }
     }
 })
 
-// const me = new user({
-//     name : 'Baibhav',
-//     age: 'nice'
-// })
-
-// me.save().then(() =>{
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
-
-const task = mongoose.model('Task', {
-    description: {
-        type : String
-    },
-    completed: {
-        type : Boolean
-    }
+const me = new user({
+    name : 'Baibhav',
+    age: 'nice'
 })
 
-const 
+me.save().then(() =>{
+    console.log(me)
+}).catch((error) => {
+    console.log('Error!', error)
+})
+
+// const task = mongoose.model('Task', {
+//     description: {
+//         type : String
+//     },
+//     completed: {
+//         type : Boolean
+//     }
+// })
+
+// const work = new task({
+//     description : "water the plant",
+//     completed : false
+// })
+
+// work.save().then(() =>{
+//     console.log(work)
+// }).catch((error) =>{
+//     console.log('Error!',  error)
+// })
