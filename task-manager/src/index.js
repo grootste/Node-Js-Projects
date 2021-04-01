ab2b8d133510a4372605fcf1d295fc0061d7bf63
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-04-01 15:09:48 +0530

    setup endpoint to test in postman

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
+
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,12 +13,68 @@ app.post('/users', (req, res)=>{
     const user = new User(req.body)
 
     user.save().then(() =>{
-        res.send(user)
-    }).catch(()=>{
-
+        res.status(201).send(user)
+    }).catch((e)=>{
+        res.status(400).send(e)
     })
 
 })
+
+app.get('/users', (req, res)=>{
+    User.find({}).then((users)=>{
+        res.status(201).send(users)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) =>{
+    const _id= req.params.id
+
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) =>{
+        res.status(500).send()
+    })
+})
+
+app.post('/tasks', (req, res) =>{
+    const task = new Task(req.body)
+
+    task.save().then(()=>{
+        res.status().send(task)
+    }).catch((e)=>{
+        res.status(400).send(e)
+    })
+})
+
+app.get('/tasks', (req, res) =>{
+    Task.find({}).then((tasks)=>{
+        res.status(201).send(tasks)
+    }).catch((e)=>{
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res)=>{
+    const _id = req.params.id
+
+    
+    Task.findById(_id).then((task)=>{
+        if (!task){
+            return res.status(404).send()
+        }
+        res.send(task)
+
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })
+})
+
 app.listen(port, () =>{
     console.log('Server is up on ' + port)
 })
