c4a93a756dc1589b9b70321fadf72c2903ea4a81
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-04-04 23:06:00 +0530

    include bcypt js in task manager app

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -14,3 +14,17 @@ app.use(taskRouter)
 app.listen(port, () =>{
     console.log('Server is up on ' + port)
 })
+
+const bcrypt = require('bcryptjs')
+const myFunction = async() =>{
+    const password = 'Singh12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare(password, hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
