7795731e731a58dacef7443ad1ceed3fcb3e53d2
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-04-07 00:10:36 +0530

    update

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -7,6 +7,21 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) =>{
+//     if(req.method === "GET"){
+//             res.send("GET request are disabled")
+//     }
+//     else{
+//         next()
+//     }
+// })
+
+// app.use((req, res, next) =>{
+//     if(req.method ==="POST"){
+//         res.status(503).send("Server Under Maintainence")
+//     }
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -20,7 +35,7 @@ const jwt = require('jsonwebtoken')
 
 const myFunction = async() =>{
      const token = jwt.sign({ _id: 'abcd123' }, 'thisismynewcourse', { expiresIn: '7 days'})
-     console.log(token)
+    console.log(token)
 
     const data = jwt.verify(token, 'thisismynewcourse')
     console.log(data)
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 
 
-router.post('/users', async (req, res)=>{
+router.post('/users',   async (req, res)=>{
     const user = new User(req.body)
 
     try{
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
     }catch(e){
         res.send(400).send(e)
     }
@@ -33,22 +35,27 @@ router.post('/users/login', async (req, res)=>{
        }
 })
 
-router.get('/users', async (req, res)=>{
 
-    try{
-        const users = await User.find({})
-        res.send(users)
-    } catch(e) {
-            res.status(500).send()
+// router.get('/users', auth,  async (req, res)=>{
 
-    }
+//     try{
+//         const users = await User.find({})
+//         res.send(users)
+//     } catch(e) {
+//             res.status(500).send()
+
+//     }
 
 
-    // User.find({}).then((users)=>{
-    //     res.status(201).send(users)
-    // }).catch((e)=>{
-    //     res.status(500).send()
-    // })
+//     // User.find({}).then((users)=>{
+//     //     res.status(201).send(users)
+//     // }).catch((e)=>{
+//     //     res.status(500).send()
+//     // })
+// })
+
+router.get('/users/me', auth,  async (req, res)=>{
+        res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) =>{
