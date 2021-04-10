921f0adeec0419a73e7ac588df949c2934b2ba35
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-04-10 23:56:14 +0530

    update

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,11 +2,13 @@
  const express = require('express')
 
  const app =express()
+ cosnt 
 
  const port = process.env.PORT || 3000
 
  const publicDirectoryPath=  path.join(__dirname, '../public')
+ console.log("hello from the other side")
  
  app.listen(port, ()=>{
-     console.log('Server is up on'+ port)
+     console.log('Server is up on '+ port)
  })
--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const { findById } = require('../models/task')
+const auth = require('../middleware/auth')
 const Task = require('../models/task')
 const router = new express.Router()
 
 
-router.post('/tasks', async (req, res) =>{
-    const task = new Task(req.body)
+router.post('/tasks', auth, async (req, res) =>{
+   //const task = new Task(req.body)
+
+   const task = new Task({
+       ...req.body,
+       owner: req.user._id
+   })
 
     try {
         await task.save()
