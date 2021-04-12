2eee9ec3bf5c1bcc3b4d0fbcfd760d8bb835fc61
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-04-12 19:30:18 +0530

    update

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,14 +1,31 @@
  const path = require('path')
+ const http = require('http')
  const express = require('express')
+ const socketio = require('socket.io')
 
  const app =express()
- cosnt 
+ const server = http.createServer(app)
+ const io = socketio(server)
 
  const port = process.env.PORT || 3000
-
  const publicDirectoryPath=  path.join(__dirname, '../public')
- console.log("hello from the other side")
  
- app.listen(port, ()=>{
-     console.log('Server is up on '+ port)
+ app.use(express.static(publicDirectoryPath))
+
+ let count =0 
+
+ io.on('connection', (socket)=>{
+     console.log('New Websocket connection')
+
+     socket.emit('countUpdated', count)
+
+     socket.on('increment', ()=>{
+         count++
+         //socket.emit('countUpdated', count)
+        io.emit('countUpdated', count)
+     })
+ })
+ 
+ server.listen(port, () => {
+     console.log('Server is up on port '+ port)
  })
