6e229c049b85a16bb83255b8b32d8a56296fc814
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-04-13 15:17:25 +0530

    setup message box

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,10 +1,17 @@
 const socket = io()
 
-socket.on('countUpdated', (count) =>{
-    console.log('The count has been updated!!', count)
-}) 
-
-document.querySelector('#increment').addEventListener('click', ()=>{
-    console.log('Clicked')
-    socket.emit('increment')
+socket.on('message', (message)=>{
+    console.log(message)
 })
+
+// socket.on('countUpdated', (count) =>{
+//     console.log('The count has been updated!!', count)
+// }) 
+
+document.querySelector('#sendMessage').addEventListener('click', ()=>{
+    console.log('Clicked')
+    socket.emit('message')
+})
+
+
+
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -12,18 +12,18 @@
  
  app.use(express.static(publicDirectoryPath))
 
- let count =0 
 
  io.on('connection', (socket)=>{
      console.log('New Websocket connection')
 
-     socket.emit('countUpdated', count)
+    socket.emit('message', 'Welcome!!')
 
-     socket.on('increment', ()=>{
-         count++
-         //socket.emit('countUpdated', count)
-        io.emit('countUpdated', count)
-     })
+    //  socket.emit('countUpdated', count)
+
+    //  socket.on('increment', ()=>{
+    //      count++
+    //     io.emit('countUpdated', count)
+    //  })
  })
  
  server.listen(port, () => {
