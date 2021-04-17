5bef91c7a9cba6c74c517fd92c98b1d614d91c7c
Baibhav <baibhav.singh2017@vitstudent.ac.in>
2021-04-17 21:49:23 +0530

    updating css of chat app

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,20 +1,77 @@
 const socket = io()
 
+
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton =$messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
+const $messages = document.querySelector('#messages')
+
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+
 socket.on('message', (message)=>{
-    console.log('The message is ', message)
+    console.log( message)
+    const html = Mustache.render(messageTemplate,{
+        message : message.text,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
 })
 
-// socket.on('countUpdated', (count) =>{
-//     console.log('The count has been updated!!', count)
-// }) 
+socket.on('locationMessage', (message)=>{
+    console.log(message)
+    const html = Mustache.render(locationMessageTemplate,{
+        url : message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
+})
 
-document.querySelector('#message-form').addEventListener('submit', (e)=>{
+
+$messageForm.addEventListener('submit', (e)=>{
      e.preventDefault()
 
-     const message = document.querySelector('input').value
+     $messageFormButton.setAttribute('disabled', 'disabled')
 
-    socket.emit('sendMessage', message)
+    //disable
+     const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (error)=>{
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value =''
+        $messageFormInput.focus()
+
+    //enable
+        if(error){
+            return console.log(error)
+        }
+
+        console.log('Message delivered!')
+    })
 })
 
 
+$sendLocationButton.addEventListener('click', ()=>{
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser.')
+    }
+
+    $sendLocationButton.setAttribute('disabled', 'disabled')
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+        }, ()=>{ 
+            $sendLocationButton.removeAttribute('disabled')
+            console.log('The location shared!')}
+        
+        
+        )
+    })
+})
 
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,8 @@
  const http = require('http')
  const express = require('express')
  const socketio = require('socket.io')
+ const Filter = require('bad-words')
+ const { generateMessage, generateLocationMessage }= require('./utils/messages')
 
  const app =express()
  const server = http.createServer(app)
@@ -16,14 +18,30 @@
  io.on('connection', (socket)=>{
      console.log('New Websocket connection')
 
-    socket.emit('message', 'Welcome!!')
+    socket.emit('message', generateMessage('Welcome!'))
+    socket.broadcast.emit('message', generateMessage('A new User has joined!'))
 
-    socket.on('sendMessage', (message) =>{
-        io.emit('message', message)
+    socket.on('sendMessage', (message, callback) =>{
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback('Profanity is not allowed')
+        }
+
+        io.emit('message', generateMessage(message))
+        callback()
     })
 
+    socket.on('sendLocation', (coords, callback)=>{
+        io.emit('locationMessage', generateLocationMessage(`https://www.google.com/maps?q=${coords.latitude},${coords.longitude}`))     
+        callback()
+    })
+
+    socket.on('disconnect', ()=>{
+        io.emit('message', generateMessage('A user has left'))
+    })
 })   
  
  server.listen(port, () => {
-     console.log('Server is up on port '+ port)
+     console.log(`Server is up on port ${port}`)
  })
